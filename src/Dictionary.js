91faf44454983379824574a1c08e5bc6a507c13f
SoleaMD <soleart00@gmail.com>
2022-08-26 19:44:11 +0200

    Added Dictionary API and axios
    -Added Dictionary API.
    -Deleted unnecessary items.
    -Added API documentation link.
    -Added axios.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    // Documentation: https://api.dictionaryapi.dev/
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleWordSearch(event) {
