601bdf8abf2f4fdd5ae2eab1ff9731ebf4eff760
SoleaMD <soleart00@gmail.com>
2022-08-28 13:41:52 +0200

    Added new components
    -Added new components and functions to show results from the API.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -30,6 +33,7 @@ export default function Dictionary() {
           onChange={handleWordSearch}
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
